3c47611b26691a7a52d6673f7a2a1b597c7533e6
Owen <owenmcmonagle@protonmail.com>
2024-02-14 12:27:39 +0000

    PRAC-2: ast(abstract syntax tree), closures, io and challenge.

--- src/main/groovy/org/learning/Main.groovy
+++ src/main/groovy/org/learning/Main.groovy
@@ -1,15 +1,26 @@
 package org.learning
 
+import groovy.transform.Canonical
 import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import groovy.transform.TupleConstructor
+
+import java.nio.file.Files
+import java.nio.file.Path
 
 static void main(String[] args) {
     println "Hello world!"
     typing()
-    oop()
+    ast_abstract_syntax_tree()
     dataTypes()
     errorHandling()
     integerCalculator()
     floatCalculator()
+    closure()
+    collections()
+    files_io()
+    files_io_writing()
+    challengeProcessingNumbers()
 }
 
 private static void integerCalculator() {
@@ -31,10 +42,11 @@ private static void floatCalculator() {
 }
 
 
-private static void oop() {
-    def equals_1 = new MethodGenerationFromAnnotation();
-    def equals_2 = new MethodGenerationFromAnnotation();
-    println "HashCode Equals: " + (equals_1 == equals_2)
+private static void ast_abstract_syntax_tree() {
+    final Person johnDoe = new Person(firstName: "John", lastName: "Doe", age: 40)
+    final Person johnDoe_1 = new Person(firstName: "John", lastName: "Doe", age: 39)
+    final Person maryBaley = new Person(firstName: "Mary", lastName: "Baley", age: 35)
+    println "HashCode Equals: " + (johnDoe == johnDoe_1) + " | " + (johnDoe == maryBaley)
 }
 
 private static void typing() {
@@ -45,8 +57,6 @@ private static void typing() {
     println duck_typing
 }
 
-@EqualsAndHashCode
-class MethodGenerationFromAnnotation {}
 
 private static void dataTypes() {
     int age = 40
@@ -75,17 +85,16 @@ private static void dataTypes() {
         println person
 }
 
-class Person {
+@EqualsAndHashCode(includeFields = true, excludes = ['age'])
+@ToString
+@TupleConstructor
+//@Canonical // Auto includes above three
+class Person implements Serializable {
     String firstName
     String lastName
     int age
     String getFullName() {
         firstName + " " + lastName
-    }
-
-    @Override
-    String toString() {
-        return getFullName()
     }
 }
 
@@ -101,3 +110,148 @@ private static void errorHandling() {
         println "caught error message"
     }
 }
+
+private static void closure() {
+    final Person johnDoe = new Person()
+    johnDoe.setFirstName("John")
+    johnDoe.setLastName("Doe")
+    johnDoe.setAge(40)
+
+    Closure personHashCode = { Person person ->
+        println person.hashCode()
+    }
+
+    personHashCode(johnDoe)
+}
+
+private static void collections() {
+    final Person johnDoe = new Person(firstName: "John", lastName: "Doe", age: 40)
+    final Person johnDoe_1 = new Person(firstName: "John", lastName: "Doe", age: 39)
+    final Person maryBaley = new Person(firstName: "Mary", lastName: "Baley", age: 35)
+
+    def allPersons = [johnDoe, johnDoe_1, maryBaley]
+
+    assert allPersons instanceof List
+    assert allPersons.size() == 3
+    assert allPersons[2] == maryBaley
+
+    // Iterate over persons
+    allPersons.each {
+        println it
+    }
+
+    // Iterate over persons with index
+    allPersons.eachWithIndex { Person entry, int i ->
+        println entry.toString() + " at " + i + " index"
+    }
+
+    // Filter for specific
+    allPersons.find { it.lastName == 'Baley' } == maryBaley
+
+    // Transform
+    allPersons.collect { it.age <= 35 } == [false, false, true]
+
+    // Sorting elements based on criteria
+    println allPersons.sort { it.age }
+}
+
+private static void files_io() {
+    Person personFromFile = new Person()
+
+    final sourcePath = Main.class.classLoader.getResource("./")?.path
+    final resourcePath = sourcePath.substring(0, sourcePath.indexOf("groovy/")) + "resources/"
+    // Read full contents.
+    File file = new File(resourcePath + "Person.txt")
+    println file.getText("UTF-8")
+
+    // Iterate over lines
+    file.eachLine {line, num ->
+        if (num == 1)
+            personFromFile.setFirstName(line)
+        else if (num == 2)
+            personFromFile.setLastName(line)
+        else if (num == 3)
+            personFromFile.setAge(line.toInteger())
+    }
+    println personFromFile
+}
+
+private static void files_io_writing() {
+    final Person personToFile = new Person(firstName: "Mary", lastName: "Baley", age: 35)
+
+    final sourcePath = Main.class.classLoader.getResource("./")?.path
+    // Remove excess path information
+    final resourcePath = sourcePath.substring(0, sourcePath.indexOf("groovy/")) + "resources/"
+    println "File Path:" + resourcePath
+    // Write to file.
+    final File file = new File(resourcePath + "mary-baley.txt")
+    file.withWriter {
+        it.writeLine personToFile.firstName
+        it.writeLine personToFile.lastName
+        it.writeLine personToFile.age.toString()
+    }
+
+    file.append("2")
+    file << "3"
+
+    // Serialization
+
+    final File byteCodeFile = new File(resourcePath + "java_bytecode_file.bin")
+    byteCodeFile.withObjectOutputStream {
+        it.writeObject(personToFile)
+    }
+}
+
+/**
+ * -> Read all lines of every file found in the "resources"
+ *    directory.
+ *
+ * -> Capture all numbers in a list.
+ *       -> Omit other entries.
+ *
+ * -> Determine highest number in List and print it.
+ *
+ * -> Add all number in List and print sum.
+ */
+private static void challengeProcessingNumbers() {
+
+    final sourcePath = Main.class.classLoader.getResource("./")?.path
+    // Remove excess path information
+    final resourcePath = sourcePath.substring(0, sourcePath.indexOf("groovy/")) + "resources/"
+    println "Resources Path:" + resourcePath
+
+    final List<Path> resources = Files.list(Path.of(resourcePath)).collect()
+    final List<Integer> numbers = []
+
+    resources.each {path ->
+
+        def file = new File(path.toString())
+
+        numbers.addAll file.readLines().collect {
+
+            try {
+                it?.toInteger()
+            } catch (NumberFormatException errorToIgnore) {
+                println "Not a number line"
+                // do nothing
+            }
+        }.findAll { it != null }
+
+    }
+
+    numbers.sort {itOne, itTwo ->
+        if (itOne == itTwo)
+            return 0
+        else if (itOne < itTwo)
+            return 1
+        else if (itOne > itTwo)
+            return -1
+    }
+
+    println "Highest number: " + numbers[0]
+
+
+    println "Sum: " + numbers.sum()
+
+}
+
